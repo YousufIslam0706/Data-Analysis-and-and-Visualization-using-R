072355eff3ebd9f12d43e78c2f9988804cbd4685
Yousuf Islam <yousuf.23220706@bau.edu.bd>
2026-03-04 17:13:03 +0600

    Add files via upload

---
+++ Class 2/3. Class 2 (Data Import - Export).R
@@ -0,0 +1,6 @@
+"","Name","Age","City","Income","Height"
+"1","John",25,"New York",50000,175
+"2","Jane",28,"Los Angeles",55000,160
+"3","Paul",35,"Chicago",65000,180
+"4","Anna",22,"Miami",45000,155
+"5","Lucas",30,"Boston",60000,170
